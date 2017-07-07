4a5f0dd9a1c86a435f3367aed2b561c79e7a70a3
熊超 <3493066520@qq.com>
2017-07-07 17:40:54 +0800

    提交配置

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+//导入工具包 require('node_modules里对应模块')
+var gulp = require('gulp'); //本地安装gulp所用到的地方
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');//压缩css插件
+var cleancss = require('gulp-clean-css');//压缩css插件
+ 
+//定义一个testLess任务（自定义任务名称）
+gulp.task('sass', function () {
+    gulp.src('./sass/**/*.scss') //该任务针对的文件
+        .pipe(sass()) //该任务调用的模块
+        .pipe(cleancss({
+            advanced: false,//类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            compatibility: 'ie7',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+            keepBreaks: true,//类型：Boolean 默认：false [是否保留换行]
+            keepSpecialComments: '*'
+            //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+        }))
+        .pipe(gulp.dest('./css')); //将会在css文件夹下生成index.css（复制文件目录及文件）
+});
+ 
+gulp.task('default',['sass']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+ 
+//gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
+//gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组) 
+//gulp.dest(path[, options]) 处理完后文件生成路径
